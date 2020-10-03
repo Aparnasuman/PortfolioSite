49281a7bcf05e9ac17477b00e2fcbaaf17a16b5a
Aparna Suman <Aparnasuman2@gmail.com>
2020-10-03 22:49:12 +0530

    final editing done

--- src/Intrestmain.js
+++ src/Intrestmain.js
@@ -12,7 +12,7 @@ return (
                     <h2 className="mb-5">Interests</h2>
       
                     <p>Apart from being a web developer, I enjoy most of the time to develop a new idea in front end animation with reference from google</p>
-                    <p className="mb-0">When forced indoors, I follow many sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                    <p className="mb-0 mb-g">When forced indoors, I follow many sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
                 </div>
             </section>
       </div>
