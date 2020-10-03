a9502f87c71426fc10ca00d7556f21a9df2ca9cc
Aparna Suman <Aparnasuman2@gmail.com>
2020-10-03 21:55:30 +0530

    routing problm solvd on icon

--- src/about.js
+++ src/about.js
@@ -4,7 +4,6 @@ import AboutMain from './aboutdetail';
 import ExperienceMain from './ExperienceDetail';
 import EducationMain from './educationDetail';
 import SkillMain from './skillsDetail';
-import NavBar  from'./nav';
 import IntrestDetail    from './Intrestmain';
 import ProjectDetail from './ProjectMain';
 
